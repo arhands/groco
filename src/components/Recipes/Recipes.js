58a1da79ac8f37f8ea581691ec68e91a6d99b93a
Andrew Hands <andrewrhands@gmail.com>
2022-01-10 16:28:13 -0600

    added datatable for recipes

--- src/components/Recipes/Recipes.js
+++ src/components/Recipes/Recipes.js
@@ -3,23 +3,53 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import { Link } from 'react-router-dom';
 import './Recipes.css'
+import DataTable from 'react-data-table-component';
 
 function Recipes() {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [confirmPassword, setConfirmPassword] = useState('');
+    function ShowRecipeDetails(id)
+    {
 
-    function validateForm() {
-        return email.length > 0 && password.length > 0;
     }
+    function AddRecipe(id)
+    {
 
-    function handleSubmit(event) {
-        event.preventDefault();
+    }
+    const columns = [
+        {name: 'Name', selector: row => row.name},
+        {name: 'Author', selector: row => row.creator},
+        {name: 'Details', selector: row => row.details},
+        {name: 'Add', selector: row => row.add},
+    ];
+    // raw data collected from server
+    const RawData = [
+        { creator: "Kyle",      name: "Water Soup",      id: 337},
+        { creator: "Steve",     name: "Pickle Sandwich", id: 561},
+        { creator: "Will",      name: "Egg on Bread",    id: 849},
+        { creator: "Christine", name: "Rice",            id: 123}
+    ];
+    // converting data into something usable.
+    const data = []
+    for(let i = 0; i < RawData.length; i++)
+    {
+        data.push({
+            id: i,
+            creator: RawData[i].creator,
+            name: RawData[i].name,
+            details: (
+                <Button onClick={() => ShowRecipeDetails(RawData[i].id)}>
+                    Details
+                </Button>
+            ),
+            add: (
+                <Button onClick={() => AddRecipe(RawData[i].id)}>
+                    Add
+                </Button>
+            )
+        });
     }
     return (
         <div className="Recipes">
-            <Form onSubmit={handleSubmit}>
-            </Form>
+            <DataTable columns={columns} data={data}/>
         </div>
     )
 }
