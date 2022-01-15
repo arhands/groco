886ee538a9c247b9f136d8ad7e18e3976b40831d
Andrew Hands <andrewrhands@gmail.com>
2022-01-14 18:16:09 -0600

    made fields editable

--- src/components/ViewRecipe/ViewRecipe.js
+++ src/components/ViewRecipe/ViewRecipe.js
@@ -7,16 +7,17 @@ import { Link } from 'react-router-dom';
 import './ViewRecipe.css'
 import DataTable from 'react-data-table-component';
 import { ButtonGroup, Dropdown, DropdownButton } from "react-bootstrap";
+import $ from 'jquery';
 
 function ViewRecipe() {
     const location = useLocation()
-    const { id, RecipeName } = location.data
+    let { id, RecipeName } = location.data
     console.log(JSON.stringify(location))
     let columns = [
-        { name: "Item",         selector: row => row.name },
-        { name: "Amount",       selector: row => row.quantity},
-        { name: "Measurement",  selector: row => row.measurement_type},
-        { name: "✓",            selector: row => row.cb},
+        { name: (<Form.Label>Item</Form.Label>),         selector: row => row.name,              sortable: true },
+        { name: (<Form.Label>Amount</Form.Label>),       selector: row => row.quantity,          sortable: true },
+        { name: (<Form.Label>Measurement</Form.Label>),  selector: row => row.measurement_type,  sortable: true },
+        { name: (<Form.Label>Include</Form.Label>),      selector: row => row.cb,                sortable: true },
     ];
     // will be replaced with DB query.
     let instructions = "ERROR"
@@ -56,22 +57,22 @@ function ViewRecipe() {
     }
     function GenerateMealPlanSelection()
     {
-        return (<></>)
         let mealPlans = [
-            {name: "3 days plan",   id: "1"},
-            {name: "1 week plan",   id: "2"},
-            {name: "Vegetarian",    id: "3"},
-            {name: "Thanksgiving",  id: "4"},
-            {name: "Weekend BBQ",   id: "5"}
+            {name: "3 days plan",   id: 1},
+            {name: "1 week plan",   id: 2},
+            {name: "Vegetarian",    id: 3},
+            {name: "Thanksgiving",  id: 4},
+            {name: "Weekend BBQ",   id: 5}
         ];
-        function AddToMealPlan(id)
+        function AddToMealPlan(p)
         {
-            //console.log("Adding recipe to meal plan " + toString(id) + ".")
+            console.log("Adding recipe to meal plan " + JSON.stringify(p) + ".")
+            //console.log("Adding recipe to meal plan " + toString(p.id) + ".")
         }
         return (
             <DropdownButton as={ButtonGroup} title="+ Meal Plan">
                 {mealPlans.map(
-                    p => (<Dropdown.Item as="button" onClick={AddToMealPlan(p.id)} eventKey={p.id}>{p.name}</Dropdown.Item>))
+                    p => (<Dropdown.Item as={Button} onClick={() => AddToMealPlan(p)} eventKey={p.id}>{p.name}</Dropdown.Item>))
                 }
             </DropdownButton>
         )
@@ -82,28 +83,50 @@ function ViewRecipe() {
 
     }
     let canEdit = false
+    let isInEditMode = false
+    function HandleEditToggle()
+    {
+        $("#input-name,#input-instructions").prop("disabled",isInEditMode)
+        $("#ingredients").find(":input").prop("disabled",isInEditMode)
+        isInEditMode = !isInEditMode
+        $("#editBtn").html(isInEditMode? "Save" : "Edit")
+    }
     //
+    let data = []
     for(let i = 0; i < ingredients.length; i++)
     {
+        data.push({
+            cb: (<Button variant="Secondary" onClick={ToggleInclude} id={"toggleBtn-" + i}>Remove</Button>),
+            name: (<input type="text" defaultValue={ingredients[i].name} onChange={e => ingredients[i].name=e.target.value} disabled/>),
+            quantity: (<input type="number" defaultValue={ingredients[i].quantity} onChange={e => ingredients[i].quantity=e.target.value} disabled/>),
+            measurement_type: (<input type="text" defaultValue={ingredients[i].measurement_type} onChange={e => ingredients[i].measurement_type=e.target.value} disabled/>)
+        });
         ingredients[i].isIncluded = true
-        ingredients[i].cb = (<input type="checkbox" defaultChecked onChange={(e) => ingredients[i].isIncluded = e.target.value}/>)
+        //ingredients[i].cb = (<input type="checkbox" className="form-check-input" defaultChecked onClick={(e) => ingredients[i].isIncluded = e.target.value}/>)
+        function ToggleInclude()
+        {
+            ingredients[i].isIncluded = !ingredients[i].isIncluded
+            $("#toggleBtn-" + i).html(ingredients[i].isIncluded? "Remove" : "Add")
+            console.log($("#toggleBtn-" + i).length)
+        }
     }
     return (
         <form>
             <label>
                 Name:
-                <input type="text" value={RecipeName} onChange={(e) => RecipeName=e.target.value} disabled/>
+                <input type="text" defaultValue={RecipeName} onChange={e => RecipeName=e.target.value} id="input-name" disabled/>
             </label>
             <br/>
-            <label>
-                Instructions:
-                <textarea type="text" value={RecipeName} onChange={(e) => instructions=e.target.value} disabled/>
-            </label>
+            <Form.Label>
+                Instructions:<br/>
+            </Form.Label>
+            <Form.Control as="textarea" rows="8" defaultValue={instructions} onChange={e => instructions=e.target.value} id="input-instructions" disabled/>
+            
             <br/>
-            <label>
+            <label id="ingredients">
                 Ingredients:
                 <br/>
-                <DataTable columns={columns} data={ingredients}/>
+                <DataTable columns={columns} data={data}/>
             </label>
             <br/>
             {
@@ -111,7 +134,7 @@ function ViewRecipe() {
                 (
                     <ButtonGroup>              
                         <Button variant="Secondary">Delete</Button>
-                        <Button variant="Secondary">Edit</Button>
+                        <Button variant="Secondary" onClick={HandleEditToggle} id="editBtn">Edit</Button>
                         <Button variant="Secondary">Close</Button>
                         {GenerateMealPlanSelection()}
                         <Button variant="Primary">+ List</Button>
